09ea3fc5eaf787d50da68af00c3c6af2987396b4
Henning Jansen <henning.jansen@uibk.ac.at>
2023-10-17 12:56:43 +0200

    Only do kron-radius analysis if not already done
    Former-commit-id: 73b7ae84e66e8a096a87c93a853ec1695417b34e

--- Scripts/kron_radius_blending.py
+++ Scripts/kron_radius_blending.py
@@ -41,72 +41,73 @@ method = sys.argv[4]
 
 catalog = ascii.read(path + "/shear_catalog.dat")
 
-for i in range(scenes):
-    for j in range(shears):
-        if method == "LF":
-            cancel = 4
-        else:
-            cancel = 1
-        for k in range(cancel):
+if "kron_blend" not in catalog.colnames:
+    for i in range(scenes):
+        for j in range(shears):
             if method == "LF":
-                catalog_cut = catalog[(catalog["scene_index"] == i) & (catalog["shear_index"] == j) & (catalog["cancel_index"] == k)]
+                cancel = 4
             else:
-                catalog_cut = catalog[
-                    (catalog["scene_index"] == i) & (catalog["shear_index"] == j)]
+                cancel = 1
+            for k in range(cancel):
+                if method == "LF":
+                    catalog_cut = catalog[(catalog["scene_index"] == i) & (catalog["shear_index"] == j) & (catalog["cancel_index"] == k)]
+                else:
+                    catalog_cut = catalog[
+                        (catalog["scene_index"] == i) & (catalog["shear_index"] == j)]
 
-            blends = []
-            positions = np.vstack([catalog_cut["position_x"], catalog_cut["position_y"]]).T
-            dist, ind = do_kdtree(positions, positions, k=[2])
-            for l in range(len(catalog_cut)):
-                ellipses = [(catalog_cut["position_x"][l], catalog_cut["position_y"][l],
-                             catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l],
-                             catalog_cut["b_image"][l] * catalog_cut["kron_radius"][l], catalog_cut["elongation"][l]),
-                            (catalog_cut["position_x"][ind[l]], catalog_cut["position_y"][ind[l]],
-                             catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]],
-                             catalog_cut["b_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]],
-                             catalog_cut["elongation"][ind[l]])]
+                blends = []
+                positions = np.vstack([catalog_cut["position_x"], catalog_cut["position_y"]]).T
+                dist, ind = do_kdtree(positions, positions, k=[2])
+                for l in range(len(catalog_cut)):
+                    ellipses = [(catalog_cut["position_x"][l], catalog_cut["position_y"][l],
+                                 catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l],
+                                 catalog_cut["b_image"][l] * catalog_cut["kron_radius"][l], catalog_cut["elongation"][l]),
+                                (catalog_cut["position_x"][ind[l]], catalog_cut["position_y"][ind[l]],
+                                 catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]],
+                                 catalog_cut["b_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]],
+                                 catalog_cut["elongation"][ind[l]])]
 
-                try:
-                    a, b = ellipse_polyline(ellipses)
-                    x, y = intersections(a, b)
-                    blends.append(1)
-
-                except AttributeError:
-                    connection_vector = np.array([catalog_cut["position_x"][l] - catalog_cut["position_x"][ind[l]],
-                                                  catalog_cut["position_y"][l] - catalog_cut["position_y"][ind[l]]]).reshape(2)
-
-                    ellip1a = np.array([catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l]
-                                        * np.cos(catalog_cut["elongation"][l] * np.pi / 180),
-                                        catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l]
-                                        * np.sin(catalog_cut["elongation"][l] * np.pi / 180)])
-
-                    ellip2a = np.array([catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]]
-                                        * np.cos(catalog_cut["elongation"][ind[l]] * np.pi / 180),
-                                        catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]]
-                                        * np.sin(catalog_cut["elongation"][ind[l]] * np.pi / 180)])
-                    v_norm = np.sqrt(sum(connection_vector ** 2))
-
-                    proj_of_u_on_v = (np.dot(connection_vector, ellip1a) / v_norm ** 2) * connection_vector
-
-                    norm = np.sqrt(sum(proj_of_u_on_v ** 2))
-
-                    proj_of_u_on_v2 = (np.dot(connection_vector, ellip2a) / v_norm ** 2) * connection_vector
-
-                    norm2 = np.sqrt(sum(proj_of_u_on_v2 ** 2))
-
-                    if (norm >= v_norm) or (norm2 >= v_norm):
+                    try:
+                        a, b = ellipse_polyline(ellipses)
+                        x, y = intersections(a, b)
                         blends.append(1)
-                    else:
-                        blends.append(0)
+
+                    except AttributeError:
+                        connection_vector = np.array([catalog_cut["position_x"][l] - catalog_cut["position_x"][ind[l]],
+                                                      catalog_cut["position_y"][l] - catalog_cut["position_y"][ind[l]]]).reshape(2)
+
+                        ellip1a = np.array([catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l]
+                                            * np.cos(catalog_cut["elongation"][l] * np.pi / 180),
+                                            catalog_cut["a_image"][l] * catalog_cut["kron_radius"][l]
+                                            * np.sin(catalog_cut["elongation"][l] * np.pi / 180)])
+
+                        ellip2a = np.array([catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]]
+                                            * np.cos(catalog_cut["elongation"][ind[l]] * np.pi / 180),
+                                            catalog_cut["a_image"][ind[l]] * catalog_cut["kron_radius"][ind[l]]
+                                            * np.sin(catalog_cut["elongation"][ind[l]] * np.pi / 180)])
+                        v_norm = np.sqrt(sum(connection_vector ** 2))
+
+                        proj_of_u_on_v = (np.dot(connection_vector, ellip1a) / v_norm ** 2) * connection_vector
+
+                        norm = np.sqrt(sum(proj_of_u_on_v ** 2))
+
+                        proj_of_u_on_v2 = (np.dot(connection_vector, ellip2a) / v_norm ** 2) * connection_vector
+
+                        norm2 = np.sqrt(sum(proj_of_u_on_v2 ** 2))
+
+                        if (norm >= v_norm) or (norm2 >= v_norm):
+                            blends.append(1)
+                        else:
+                            blends.append(0)
 
 
-            col_c = Column(name='kron_blend', data=blends)
+                col_c = Column(name='kron_blend', data=blends)
 
-            catalog_cut.add_column(col_c)
+                catalog_cut.add_column(col_c)
 
-            if (i == 0) and (j == 0) and (k == 0):
-                catalog_out = catalog_cut
-            else:
-                catalog_out = vstack([catalog_out, catalog_cut])
+                if (i == 0) and (j == 0) and (k == 0):
+                    catalog_out = catalog_cut
+                else:
+                    catalog_out = vstack([catalog_out, catalog_cut])
 
-ascii.write(catalog_out, path + "/shear_catalog.dat", overwrite=True)
+    ascii.write(catalog_out, path + "/shear_catalog.dat", overwrite=True)
